6ab5dab9f7572c5e966064de4ce84731405c85bc
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-17 22:30:20 -0300

    chore: add spl fixed array example

---
+++ aula1.php
@@ -0,0 +1,13 @@
+<?php
+
+$musicas = new SplFixedArray(2);
+
+$musicas[0] = 'One Dance';
+$musicas[1] = 'Closer';
+
+$musicas->setSize(4);
+
+$musicas[2] = 'Rockstar';
+$musicas[3] = 'Love Yourself';
+
+echo $musicas->getSize() . PHP_EOL;

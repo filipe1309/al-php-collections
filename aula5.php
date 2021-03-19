734f4e679dae1fc3673bda4d38021e8d1946a082
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-03-18 22:12:58 -0300

    chore: map with SplObjectStorage

--- aula5.php
+++ aula5.php
@@ -14,4 +14,24 @@ $scorpion = new Album('Scorpion');
 $albuns->attach($scorpion);
 $albuns->attach($scorpion);
 
-var_dump($albuns);
+$musicasDivide = new SplFixedArray(2);
+$musicasDivide[0] = new Musica('musicasDivide 1');
+$musicasDivide[1] = new Musica('musicasDivide 2');
+
+$musicasScorpion = new SplFixedArray(3);
+$musicasScorpion[0] = new Musica('musicasScorpion 1');
+$musicasScorpion[1] = new Musica('musicasScorpion 2');
+$musicasScorpion[2] = new Musica('musicasScorpion 3');
+
+$albuns[$divide] = $musicasDivide;
+$albuns[$scorpion] = $musicasScorpion;
+
+foreach ($albuns as $album) {
+  echo "Album: " . $album->getNome() . PHP_EOL;
+
+  foreach ($albuns[$album] as $musica) {
+    echo "Musica: " . $musica->getNome() . PHP_EOL;
+  }
+
+  echo PHP_EOL;
+}
